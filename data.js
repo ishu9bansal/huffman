b5319e5886b18548b432ac12fd3daa93003104a5
ishu9bansal <ishubansal1400@gmail.com>
2021-02-28 18:48:03 +0530

    create huffman tree from text

---
+++ data.js
@@ -0,0 +1,50 @@
+var text;
+var frequency;
+var root;
+function getFrequency(text){
+	f = {};
+	t = Array.from(text);
+	t.forEach(function(x){
+		if(!f[x])	f[x] = 0;
+		f[x]++;
+	});
+	return f;
+}
+function getHuffmanTree(f){
+	var nodes = [];
+	for(var k in f){
+		nodes.push({
+			value: f[k],
+			children: [
+				{
+					name: k
+				}
+			]
+		});
+	}
+	nodes.sort((a,b) => b.value-a.value);
+	var p = nodes.pop();
+	while(nodes.length){
+		var q = nodes.pop();
+		node = {
+			value: p.value+q.value,
+			children: [p,q]
+		};
+		nodes.push(node);
+		var l = nodes.length-1;
+		while(l&&nodes[l].value>nodes[l-1].value){
+			var temp = nodes[l];
+			nodes[l] = nodes[l-1];
+			nodes[l-1] = temp;
+			l--;
+		}
+		p = nodes.pop();
+	}
+	return p;
+}
+
+function setUp(t){
+	text = t;
+	frequency = getFrequency(text);
+	root = getHuffmanTree(frequency);
+}
